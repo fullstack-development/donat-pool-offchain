7b8bf9f8ea9ada5e1b6d0980a691c7c0f3cf4936
olgaklimenko <oklimenko92@mail.ru>
2023-01-24 19:34:13 +0700

    change Datum

--- src/Protocol/Datum.purs
+++ src/Protocol/Datum.purs
@@ -2,23 +2,22 @@ module Protocol.Datum where
 
 
 
-import Contract.PlutusData 
-  ( class HasPlutusSchema
-  , class ToData
-  , type (:+)
-  , type (:=)
-  , type (@@)
-  , I
-  , PNil
-  , Z
-  , genericToData
-  )
+import Contract.PlutusData (class HasPlutusSchema, type (:+), type (:=), type (@@), I, PNil, Z, genericToData)
 import Contract.Prelude (class Generic)
 import Data.BigInt (BigInt)
 import Data.Rational (Ratio)
-import Ctl.Internal.Types.UsedTxOuts (TxOutRefCache)
 import Contract.Address (PaymentPubKeyHash)
 import Contract.Value (CurrencySymbol, TokenName)
+import Data.Lens (Lens')
+import Prelude (class Eq, class Ord, (<<<))
+import Data.Lens.Iso.Newtype (_Newtype)
+import Data.Lens.Record (prop)
+import Type.Proxy (Proxy(Proxy))
+import Data.Newtype (class Newtype)
+import Ctl.Internal.FromData
+import Ctl.Internal.ToData (class ToData)
+import Ctl.Internal.Types.Transaction (TransactionHash)
+import Data.UInt (UInt)
 
 newtype PPoolSizeLimits = PPoolSizeLimits {
     minAmount :: BigInt,
@@ -38,8 +37,13 @@ instance
         :+ PNil
     )
 
+derive newtype instance Eq PPoolSizeLimits
+derive newtype instance Ord PPoolSizeLimits
 instance ToData PPoolSizeLimits where
   toData = genericToData
+
+instance FromData PPoolSizeLimits where
+  fromData = genericFromData
 
 newtype PDurationLimits = PDurationLimits {
     minDuration :: BigInt,
@@ -59,8 +63,14 @@ instance
         :+ PNil
     )
 
+derive newtype instance Eq PDurationLimits
+derive newtype instance Ord PDurationLimits
 instance ToData PDurationLimits where
   toData = genericToData
+
+instance FromData PDurationLimits where
+  fromData = genericFromData
+
 
 newtype PProtocolConfig = PProtocolConfig {
     protocolFee :: Ratio BigInt,
@@ -82,19 +92,97 @@ instance
         :+ PNil
     )
 
+derive newtype instance Eq PProtocolConfig
+derive newtype instance Ord PProtocolConfig
 instance ToData PProtocolConfig where
   toData = genericToData
 
+instance FromData PProtocolConfig where
+  fromData = genericFromData
+
+newtype PTokenOrigin = PTokenOrigin {
+  txId :: TransactionHash,
+  index :: UInt
+}
+
+derive instance Generic PTokenOrigin _
+derive newtype instance Eq PTokenOrigin
+derive newtype instance Ord PTokenOrigin
+
+instance
+  HasPlutusSchema
+    PTokenOrigin
+    ( "PTokenOrigin" :=
+              ( "txId"  := I TransactionHash
+               :+ "index" := I UInt
+              :+ PNil)
+           @@ Z
+        :+ PNil
+    )
+
+instance ToData PTokenOrigin where
+  toData = genericToData
+
+instance FromData PTokenOrigin where
+  fromData = genericFromData
+
 newtype PProtocolConstants = PProtocolConstants {
     managerPkh :: PaymentPubKeyHash,
-    tokenOriginRef :: TxOutRefCache,
+    tokenOrigin :: PTokenOrigin,
     protocolCurrency :: CurrencySymbol,
     protocolTokenName :: TokenName
 }
 
 derive instance Generic PProtocolConstants _
+derive newtype instance Eq PProtocolConstants
+derive newtype instance Ord PProtocolConstants
+
+instance
+  HasPlutusSchema
+    PProtocolConstants
+    ( "PProtocolConstants" :=
+              ( "managerPkh"  := I PaymentPubKeyHash
+               :+ "tokenOrigin" := I PTokenOrigin
+               :+ "protocolCurrency" := I CurrencySymbol
+               :+ "protocolTokenName" := I TokenName
+              :+ PNil)
+           @@ Z
+        :+ PNil
+    )
+
+instance ToData PProtocolConstants where
+  toData = genericToData
+
+instance FromData PProtocolConstants where
+  fromData = genericFromData
 
 newtype PProtocolDatum = PProtocolDatum {
     protocolConstants :: PProtocolConstants,
     protocolConfig :: PProtocolConfig
 }
+
+_protocolConstants :: Lens' PProtocolDatum PProtocolConstants
+_protocolConstants = _Newtype <<< prop (Proxy :: Proxy "protocolConstants")
+
+derive instance Generic PProtocolDatum _
+derive instance Newtype PProtocolDatum _
+derive newtype instance Eq PProtocolDatum
+derive newtype instance Ord PProtocolDatum
+
+instance
+  HasPlutusSchema
+    PProtocolDatum
+    ( "PProtocolDatum" :=
+              ( "protocolConstants"  := I PProtocolConstants
+               :+ "protocolConfig" := I PProtocolConfig
+              :+ PNil)
+           @@ Z
+        :+ PNil
+    )
+
+instance ToData PProtocolDatum where
+  toData = genericToData
+
+instance FromData PProtocolDatum where
+  fromData = genericFromData
+
