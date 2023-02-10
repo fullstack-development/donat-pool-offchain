81092fea4ccd76e5fcfcbc412b98ebe907412031
olgaklimenko <oklimenko92@mail.ru>
2023-02-10 15:58:58 +0700

    start protocol

--- ui/index.tsx
+++ ui/index.tsx
@@ -1,15 +1,19 @@
 import ReactDOM from 'react-dom/client';
 
-import offchain from '../dist'
+// import offchain from '../dist'
+
+const a = await import ('@offchain/Scaffold.Main')
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
+const startProtocolParams = {"minAmountParam":50000000,"maxAmountParam":1000000000,"minDurationParam":100,"maxDurationParam":1000,"protocolFeeParam":10}
 // offchain.resolve().then(console.log)
-console.log(offchain)
+// console.log(offchain)
+
 const App = () => {
   return (
     <div>
       <h1>Offchain integration</h1>
-      {/* <button onClick={()=>{console.log(a.runStartProtocol({"minAmountParam":50000000,"maxAmountParam":1000000000,"minDurationParam":100,"maxDurationParam":1000,"protocolFeeParam":10})().join(console.log)())}}>Connect wallet</button> */}
+      <button onClick={()=>console.log(a)}>Start Protocol</button>
     </div>
   )
 }
