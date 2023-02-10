b0f311f0d8e3ef668f49749d88b07645ea74e7a5
Svetlana Dunicheva <svetlana.dunicheva@metalamp.io>
2023-02-10 13:29:34 +0300

    added start protocol and update protocol

--- ui/index.tsx
+++ ui/index.tsx
@@ -1,21 +1,54 @@
+import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
-// import offchain from '../dist'
+const a = await import('@offchain/Scaffold.Main');
 
-const a = await import ('@offchain/Scaffold.Main')
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-const startProtocolParams = {"minAmountParam":50000000,"maxAmountParam":1000000000,"minDurationParam":100,"maxDurationParam":1000,"protocolFeeParam":10}
-// offchain.resolve().then(console.log)
-// console.log(offchain)
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 
 const App = () => {
+  const startProtocolParams = {
+    minAmountParam: 50000000,
+    maxAmountParam: 1000000000,
+    minDurationParam: 100,
+    maxDurationParam: 1000,
+    protocolFeeParam: 10,
+  };
+  const updatedParams = {
+    minAmountParam: 50000000,
+    maxAmountParam: 1000000000,
+    minDurationParam: 100,
+    maxDurationParam: 1000,
+    protocolFeeParam: 9,
+  };
+  const [protocol, setProtocol] = useState();
+
+  const onStartProtocolComplete = completedProtocol => {
+    setProtocol(completedProtocol);
+  };
+
+  const onUpdateProtocolComplete = something => {
+    console.log('update success');
+  };
+
+  const onStartProtocolClick = () => {
+    a.main.value0.startProtocol(onStartProtocolComplete)(console.log)(
+      startProtocolParams
+    )();
+  };
+  const onUpdateProtocolClick = () => {
+    a.main.value0.updateProtocol(onUpdateProtocolComplete)(console.log)(
+      protocol
+    )(updatedParams)();
+  };
+
   return (
     <div>
       <h1>Offchain integration</h1>
-      <button onClick={()=>console.log(a)}>Start Protocol</button>
+      <button onClick={onStartProtocolClick}>Start Protocol</button>
+      <button onClick={onUpdateProtocolClick}>Update Protocol</button>
     </div>
-  )
-}
+  );
+};
 
-root.render(<App/>)
+root.render(<App />);
