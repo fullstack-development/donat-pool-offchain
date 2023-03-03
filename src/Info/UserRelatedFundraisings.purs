c9eaa7c0b15632f8476da480bebcf4e540d972cb
Ekaterina Bushueva <mayflower.studio@gmail.com>
2023-03-03 14:38:27 +0300

    added getUserRelatedFundraisings

---
+++ src/Info/UserRelatedFundraisings.purs
@@ -0,0 +1,32 @@
+module Info.UserRelatedFundraisings where
+
+import Contract.Prelude
+
+import Contract.Address (ownPaymentPubKeysHashes)
+import Contract.Config (testnetNamiConfig)
+import Contract.Log (logInfo')
+import Contract.Monad (Contract, liftContractM, runContract)
+import Data.Array as Array
+import Effect.Aff (runAff_)
+import Effect.Exception (Error, message)
+import Info.AllFundraisings (getAllFundraisings)
+import Info.UserData (FundraisingInfo, filterByPkh)
+import Protocol.Models (Protocol)
+
+runGetUserRelatedFundraisings :: (Array FundraisingInfo -> Effect Unit) -> (String -> Effect Unit) -> Protocol -> Effect Unit
+runGetUserRelatedFundraisings onComplete onError protocol = runAff_ handler $
+  runContract testnetNamiConfig (getUserRelatedFundraisings protocol)
+  where
+  handler :: Either Error (Array FundraisingInfo) -> Effect Unit
+  handler (Right response) = onComplete response
+  handler (Left err) = onError $ message err
+
+getUserRelatedFundraisings :: Protocol -> Contract () (Array FundraisingInfo)
+getUserRelatedFundraisings protocol = do
+  allFrs <- getAllFundraisings protocol
+  ownHashes <- ownPaymentPubKeysHashes
+  ownPkh <- liftContractM "Impossible to get own PaymentPubkeyHash" $ Array.head ownHashes
+  logInfo' $ "Own Payment pkh is: " <> show ownPkh
+  let userFrs = filterByPkh ownPkh allFrs
+  logInfo' $ "Discovered items: " <> show userFrs
+  pure userFrs
