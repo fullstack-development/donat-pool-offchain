b14081c8a756fd41680aa0b9c3f7c300a1873492
Ekaterina Bushueva <mayflower.studio@gmail.com>
2023-03-29 15:43:10 +0300

    fixed comment

--- src/Fundraising/Calculations.purs
+++ src/Fundraising/Calculations.purs
@@ -8,7 +8,7 @@ import Shared.MinAda (minAda)
 
 calcFee :: BigInt -> BigInt -> Maybe BigInt
 calcFee feePercent funds' = do
-  fee <- mkBigIntRational (feePercent * funds' /\ fromInt 100) -- Impossible to get an error as `fromInt 100 /= 0`
+  fee <- mkBigIntRational (feePercent * funds' /\ fromInt 100) -- Impossible to get a "division by zero" error
   rounded <- roundBigIntRatio fee
   let feeAmount = max rounded minAda
   pure feeAmount
