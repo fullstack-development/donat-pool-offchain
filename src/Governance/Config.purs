5222a2945b79c5b501caddff89084a9115933a02
Olga Klimenko <oklimenko92@mail.ru>
2023-08-22 16:15:17 +0700

    rm unused function

--- src/Governance/Config.purs
+++ src/Governance/Config.purs
@@ -5,14 +5,12 @@ import Contract.Prelude
 import Contract.Monad (Contract)
 import Contract.Value (TokenName)
 import Contract.Value as Value
-import Data.Argonaut.Core (stringify)
 import Data.Argonaut.Decode (decodeJson, parseJson)
-import Data.Argonaut.Encode (encodeJson)
 import Effect (Effect)
 import Ext.Data.Either (eitherM)
 import Ext.Seriaization.Token (deserializeCurrency, deserializeTokenName)
 import Node.Encoding (Encoding(..))
-import Node.FS.Sync (readTextFile, writeTextFile)
+import Node.FS.Sync (readTextFile)
 
 type GovernanceConfig =
   { governanceCurrency :: String
@@ -25,11 +23,6 @@ readGovernanceConfig = do
   jsonContent <- liftEffect $ eitherM "Can't parse governance config file: " $ parseJson content
   (governanceConfig :: GovernanceConfig) <- liftEffect $ eitherM "Can't decode GovernanceConfig: " $ (decodeJson jsonContent)
   pure governanceConfig
-
-writeGovernanceConfig :: GovernanceConfig -> Effect Unit
-writeGovernanceConfig governanceConfig = do
-  let jGovernanceConfig = stringify $ encodeJson governanceConfig
-  liftEffect $ writeTextFile UTF8 "conf/governance.conf" jGovernanceConfig
 
 getGovTokenFromConfig ∷ Contract (Value.CurrencySymbol /\ TokenName)
 getGovTokenFromConfig = do
